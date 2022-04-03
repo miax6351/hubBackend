a378d3fccf9af8e88e15d95f3fa9076e38be2646
steveng85 <s205456@student.dtu.dk>
2022-04-03 17:13:53 +0200

    Fixed Acces-Control-Allow-Origin policy

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ models.sequelize.sync({force: false}).then(() => {
    })
 
 //middelware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use(express.json());
 
