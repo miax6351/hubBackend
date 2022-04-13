b9bca7c16f4c69fbbdf8fd8d6036923baf5ef28d
Mia <miadong01@yahoo.com>
2022-04-13 15:00:13 +0200

    post function in progress

---
+++ app/models/appointment.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, Datatypes) => {
+
+    const Appointment = sequelize.define("appointment", {
+      studentId: { type: Datatypes.STRING, allowNull: false, primaryKey: true },
+      appointmentId: { type: Datatypes.INTEGER, allowNull: true, primaryKey: true },
+      startDate: { type: Datatypes.DATE, allowNull: false},
+      endDate: { type: Datatypes.DATE, allowNull: false },
+      title: { type: Datatypes.STRING, allowNull: false},
+      courseId: { type: Datatypes.STRING, allowNull: true},
+      
+    },
+    {
+        tableName: "appointment",
+        frezeTableName: true,
+        timestamps: false
+    });
+
+    return Appointment;
+  };
+
