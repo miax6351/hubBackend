3ed1503c912e3170fa431f90179d05cc84b60062
steveng85 <s205456@student.dtu.dk>
2022-04-17 13:07:57 +0200

    installed cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //const helmet = require("helmet");
 const models = require("./app/models");
 const express = require("express");
+const cors = require("cors");
+
 
 
 
@@ -11,6 +13,7 @@ models.sequelize.sync({force: false}).then(() => {
    })
 
 //middelware
+app.use(cors())
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
