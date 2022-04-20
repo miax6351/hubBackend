5b94abdfec783ccd27e574f1640c7ba16d75bc75
Mia <miadong01@yahoo.com>
2022-04-20 15:56:43 +0200

    443

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const { version } = require("./package.json");
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => {
   console.log(`app started: ${version} Server is running on port ${PORT}.`);
 });
