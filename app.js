c66d4f7e7ffb40090919a41d57b51ae6862e9f24
Mia <miadong01@yahoo.com>
2022-04-20 20:33:49 +0200

    fjernet

--- app.js
+++ app.js
@@ -29,13 +29,6 @@ app.get('/', (req, res) => {
   res.json({ message: "Welcome to StudentHub" });
 });
 
-app.get('/*', function(req, res) {   
-  res.sendFile(path.join(__dirname, '/var/www/studenthub.bhsi.xyz/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
 
 //app.use(helmet());
 //announcements
