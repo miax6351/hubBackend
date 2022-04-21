9feb42291dba97e5a48a66b7da863f79bf29958b
Mia <miadong01@yahoo.com>
2022-04-21 22:12:25 +0200

    8080 igen

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const { version } = require("./package.json");
 
 // set port, listen for requests
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`app started: ${version} Server is running on port ${PORT}.`);
 });
