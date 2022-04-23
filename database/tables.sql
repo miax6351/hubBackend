c11b9216825f619f037b165aceffeb2330c34422
Mia <miadong01@yahoo.com>
2022-04-23 11:20:24 +0200

    sql fix

--- database/tables.sql
+++ database/tables.sql
@@ -1,38 +1,38 @@
 CREATE TABLE student (
-  id varchar(25) PRIMARY KEY,
-  firstName varchar(25),
-  lastName varchar(25),
-  mail varchar(25),
-  studyclassId varchar(25)
+  id varchar(50) PRIMARY KEY,
+  firstName varchar(50),
+  lastName varchar(50),
+  mail varchar(50),
+  studyclassId varchar(50)
 );
 
 CREATE TABLE instructor (
-  id varchar(25) PRIMARY KEY,
-  firstName varchar(25),
-  lastName varchar(25),
-  mail varchar(25),
-  phone varchar(10)
+  id varchar(50) PRIMARY KEY,
+  firstName varchar(50),
+  lastName varchar(50),
+  mail varchar(50),
+  phone varchar(50)
 );
 
 
 CREATE TABLE course (
   id varchar(10) PRIMARY KEY,
-  courseName varchar(25),
-  courseDescription varchar(100)),
-  ects varchar(25),
-  instructorId varchar(25),
+  courseName varchar(50),
+  courseDescription varchar(100),
+  ects varchar(50),
+  instructorId varchar(50),
   FOREIGN KEY (instructorId) REFERENCES instructor (id)
 );
 
 CREATE TABLE grades (
   gradeDK int PRIMARY KEY,
-  gradeIn varchar(25)
+  gradeIn varchar(50)
 );
 
 CREATE TABLE grade (
-  studentId varchar(25),
-  courseId int,
+  studentId varchar(50),
   gradeDK int,
+  courseId varchar(10),
   PRIMARY KEY (studentId, gradeDK, courseId),
   FOREIGN KEY (studentId) REFERENCES student(id),
   FOREIGN KEY (gradeDK) REFERENCES grades (gradeDK),
@@ -41,24 +41,24 @@ CREATE TABLE grade (
 
 
 CREATE TABLE studentCourses (
-  studentId varchar(25),
-  courseId int,
+  studentId varchar(50),
+  courseId varchar(10),
   PRIMARY KEY (studentId, courseId),
   FOREIGN KEY (studentId) REFERENCES student (id),
   FOREIGN KEY (courseId) REFERENCES course (id)
 );
 
 CREATE TABLE location (
-  buildingName varchar(25),
+  buildingName varchar(50),
   roomNumber int,
   PRIMARY KEY (buildingName, roomNumber)
 );
 
 
 CREATE TABLE lesson (
-  courseId int,
+  courseId varchar(10),
   weekNo int,
-  locationBuilding varchar(25),
+  locationBuilding varchar(50),
   locationRoom int,
   PRIMARY KEY (courseId, weekNo),
   FOREIGN KEY (courseId) REFERENCES course (id),
@@ -66,26 +66,23 @@ CREATE TABLE lesson (
 );
 
 CREATE TABLE studyprogramme (
-  programmeId varchar(25) PRIMARY KEY,
-  programmeName varchar(25)
+  programmeId varchar(50) PRIMARY KEY,
+  programmeName varchar(50)
 );
 
 CREATE TABLE studyclass (
-  id varchar(25) PRIMARY KEY,
-  semester varchar(25),
-  programmeId varchar(25),
+  id varchar(50) PRIMARY KEY,
+  semester varchar(50),
+  programmeId varchar(50),
   FOREIGN KEY (programmeId) REFERENCES studyprogramme (programmeId),
   FOREIGN KEY (id) REFERENCES student (id)
 );
 
 
-
-
-
 CREATE TABLE assignment (
   id int,
-  courseId int,
-  fileURL varchar(25),
+  courseId varchar(10),
+  fileURL varchar(50),
   openDate timestamp,
   dueDate timestamp,
   PRIMARY KEY (id, courseId),
@@ -93,36 +90,36 @@ CREATE TABLE assignment (
 );
 
 CREATE TABLE announcement (
-  studentId varchar(25),
+  studentId varchar(50),
   id int AUTO_INCREMENT,
-  title varchar(25),
-  message varchar(25),
-  sender varchar(25),
+  title varchar(50),
+  message varchar(50),
+  sender varchar(50),
   isRead boolean,
   PRIMARY KEY (studentId, id),
   FOREIGN KEY (studentId) REFERENCES student (id)
 ) ENGINE=MyISAM;
 
 CREATE TABLE appointment (
-  studentId varchar(25),
+  studentId varchar(50),
   appointmentId int AUTO_INCREMENT,
   startDate date,
   endDate date,
-  title varchar(25),
-  courseId int,
+  title varchar(50),
+  courseId varchar(10),
   PRIMARY KEY (studentId, appointmentId),
   FOREIGN KEY (studentId) REFERENCES student (id),
   FOREIGN KEY (courseId) REFERENCES lesson (courseId)
 ) ENGINE=MyISAM;
 
 CREATE TABLE lessonplan (
-  studentId varchar(25),
-  course varchar(25),
+  studentId varchar(50),
+  course varchar(50),
   weekNo int,
   date date,
-  topic varchar(25),
-  learningObjectives varchar(25),
-  litterature varchar(25),
+  topic varchar(50),
+  learningObjectives varchar(100),
+  litterature varchar(50),
   pages int,
   PRIMARY KEY (studentId, course, weekNo),
   FOREIGN KEY (studentId) REFERENCES student (id)
