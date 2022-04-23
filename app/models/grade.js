5990ed57e4250fe1d0b1fa9b1d489b6197211ec5
Mia <miadong01@yahoo.com>
2022-04-23 11:41:46 +0200

    grade changes

--- app/models/grade.js
+++ app/models/grade.js
@@ -1,21 +1,23 @@
-
-
+module.exports = (sequelize, Datatypes) => {
 const Grade = sequelize.define("grade", {
 
     studentId: {type: Datatypes.STRING, allowNull: false, primaryKey: true },
     courseId: { type: Datatypes.INTEGER, allowNull: false, primaryKey: true },
-    courseTitle: { type: Datatypes.STRING },
-    message: { type: Datatypes.TEXT },
-    sender: { type: Datatypes.STRING},
-    isRead: { type: Datatypes.BOOLEAN }
+    courseName: { type: Datatypes.STRING },
+    gradeDK: { type: Datatypes.INTEGER},
+    InternationalGrade: { type: Datatypes.STRING},
 
   },
   {
-      tableName: "announcement",
+      tableName: "grade",
       frezeTableName: true,
       timestamps: false
   });
 
-Announcement.associate = function (models) {
-    models.announcement.belongsTo(models.student)
+Grade.associate = function (models) {
+    models.grade.belongsTo(models.student)
+    models.grade.hasOne(models.course)
 }
+return Grade;
+};
+
