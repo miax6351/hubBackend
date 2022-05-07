27f2bf4e7a6b59b0261efb2de8083fad4e782ee5
steveng85 <s205456@student.dtu.dk>
2022-05-07 14:44:31 +0200

    Made get Grades for specifik user

--- app/controllers/gradeController.js
+++ app/controllers/gradeController.js
@@ -4,22 +4,23 @@ const student = require("../models/student");
 
     
     const getAllGrades = async (reg, res) => { 
-        let grades = await model.grade.findAll({
-            include: {
-                model: student, required: true, attributes:['studentId'],
-                model: course, required: true, attributes:['courseName']}
-                    
-        });
-        res.status(200).send(grades);
+         
+
+        let grade = await model.grade.findAll({
+            where: {studentId: "s205353"},
+            include: model.course
+        })
+        res.status(200).send(grade);
     }
 
     const getGrades = async (req, res) =>{
         let token = req.params.token;
         let student = await model.student.findOne({ where: {token: token} });
 
-        let grade = await model.grade.findAll(
-            {where: {studentId: student.id}}
-            )
+        let grade = await model.grade.findAll({
+            where: {studentId: student.id},
+            include: model.course
+        })
         res.status(200).send(grade);
     }
 
